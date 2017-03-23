4569dec248960bfddb75fcba46187804e9e5112f
Samuel <sciberrassamuelxibi@gmail.com>
2017-03-23 10:28:01 +0100

    New Project Created
    --Solution--

---
+++ PetrolStation_V1/PetrolStation_V1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PetrolStation_V1.MvcApplication" Language="C#" %>
---
+++ PetrolStation_V1/PetrolStation_V1/Models/registerModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetrolStation_V1.Models
+{
+    public class registerModel
+    {
+    }
+}
